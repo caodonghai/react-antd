224c937fcbb3ce7bac34940a6bb8d5aecb6630ad
caodonghai <caodonghai234@163.com>
2020-04-16 17:36:13 +0800

    基于customize-cra

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+/* 
+    *@file config-overrides
+    *基于customize和react-app-rewide的定制化配置
+*/
+
+//从customize-cra引入一些相关的方法
+const { override }  = require('customize-cra')
+
+module.exports = override()
