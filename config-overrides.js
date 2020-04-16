06c329406b411a32cebaaaafaf77f415c0395010
caodonghai <caodonghai234@163.com>
2020-04-16 18:12:35 +0800

    安装antd，安装按需加载插件bable-plugin-import

--- config-overrides.js
+++ config-overrides.js
@@ -6,11 +6,17 @@
 //从customize-cra引入一些相关的方法
 const {
     override,
-    addLessLoader
+    addLessLoader,
+    fixBabelImports
 }  = require('customize-cra')
 
 module.exports = override(
     addLessLoader({
         javascriptEnabled: true,
-    })
+    }),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
 )
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import { Button } from 'antd'
+
 class App extends Component {
     render() {
         return (
             <div>
                 app
+                <Button>测试按钮</Button>
             </div>
         );
     }
