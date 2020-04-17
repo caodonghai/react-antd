b7a01242031bfde393f6c9711e357607be3424e8
caodonghai <caodonghai234@163.com>
2020-04-17 15:31:08 +0800

    使用react-loadable实现路由懒加载；同时自己使用高阶组件实现一个loadable组件

---
+++ src/views/Myloadable.js
@@ -0,0 +1,38 @@
+//自己实现路由懒加载组件
+
+import React, { Component } from 'react';
+
+const Loadable = ({//是一个高阶组件
+    loader,
+    loading: Loading
+}) => {
+    return class Myloadable extends Component {
+        state = {
+            LoadedComponent: null
+        }
+
+        componentDidMount() {
+            //以下语句相当于执行：import('./dashboard')
+            loader()
+                .then(resp => {
+                    this.setState({
+                        LoadedComponent: resp.default,
+                    })
+                    console.log(resp.default)
+                })
+        }
+
+        render() {
+        const { LoadedComponent } = this.state;
+            return (
+                LoadedComponent
+                ?
+                <LoadedComponent />
+                :
+                <Loading />
+            );
+        }
+    }
+}
+
+export default Loadable;
--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,48 @@
-import DashBoard from './DashBoard/DashBoard'
-import Login from './Login/Login'
-import Artical from './Artical'
-import NotFound from './NotFound/NotFound'
-import Settings from './Settings/Settings'
-import ArticalEdit from './Artical/Edit'
+import Loadable from 'react-loadable'
+
+/* 下面注释的是一个简易的loadable的原理 */
+// import Loadable from './Myloadable'
+import { Loading } from '../components'
+
+// import DashBoard from './DashBoard/DashBoard'
+// import Login from './Login/Login'
+// import Artical from './Artical'
+// import NotFound from './NotFound/NotFound'
+// import Settings from './Settings/Settings'
+// import ArticalEdit from './Artical/Edit'
+
+/* 
+ *使用react-loadable按需加载路由
+*/
+const DashBoard = Loadable({
+    loader: () => import('./DashBoard/DashBoard'),
+    loading: Loading
+})
+
+const Login = Loadable({
+    loader: () => import('./Login/Login'),
+    loading: Loading
+})
+
+const Artical = Loadable({
+    loader: () => import('./Artical'),
+    loading: Loading
+})
+
+const NotFound = Loadable({
+    loader: () => import('./NotFound/NotFound'),
+    loading: Loading
+})
+
+const Settings = Loadable({
+    loader: () => import('./Settings/Settings'),
+    loading: Loading
+})
+
+const ArticalEdit = Loadable({
+    loader: () => import('./Artical/Edit'),
+    loading: Loading
+})
 
 
 export {
